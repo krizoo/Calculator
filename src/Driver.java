a5e9f1b36b9857ee5fe649bce459a864b7bb6641
krizoo <dev@whitehatboy.com>
2016-01-30 16:21:44 +0200

    *Added for loops , and array of integers

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,17 @@
 
 public class Driver {
-
-	/**
-	 * @param args
-	 */
+      private int counter = 5;
+      private Integer[] listofNumber = new Integer();
 	public static void main(String[] args) {
-		System.out.println("Hello world from Java Programming");
+
+		for (int i=0; i<counter; i++){
+				System.out.println("Hello world from Java Programming");
+				listOfNumber[i]= ++i;
+		}
+		for(Integer list : listOfNumber){
+				System.out.println(list);
+		}
+		
 	}
 
 }
